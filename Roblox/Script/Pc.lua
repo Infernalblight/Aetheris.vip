e667c6487398db836c40abb48dc7652efe525dbc
Infernalblight <129289821+Infernalblight@users.noreply.github.com>
2025-03-11 15:07:59 +0100

    Update Pc.lua

--- Roblox/Script/Pc.lua
+++ Roblox/Script/Pc.lua
@@ -1,1 +1,11 @@
-local sigma = 1
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+
+local window = DrRayLibrary:Load("Aetheris.vip", "Default")
+
+if game.PlaceId == 72857483350036 then
+local GameBattletab = DrRayLibrary.newTab("Admin (Only The Battlegrounds!)", "")
+
+GameBattletab.newButton("Admin", "Gives access to admin menu", function()
+    game.Players.LocalPlayer.PlayerGui.Settings.Settings.Visible = true
+end)
+end
