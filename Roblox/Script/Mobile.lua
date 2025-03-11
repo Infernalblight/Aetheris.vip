ff408afa48d36531df19aab9336fef65474cbaf2
Infernalblight <129289821+Infernalblight@users.noreply.github.com>
2025-03-11 09:32:27 +0100

    Update Mobile.lua

--- Roblox/Script/Mobile.lua
+++ Roblox/Script/Mobile.lua
@@ -1,1 +1,13 @@
-local sigma = 2
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+
+local window = DrRayLibrary:Load("Aetheris.vip", "Default")
+
+local GameBattletab = DrRayLibrary.newTab("Admin+", "")
+
+GameBattletab.newButton("Admin", "Gives access to admin menu", function()
+    game.Players.LocalPlayer.PlayerGui.Settings.Settings.Visible = true
+end)
+
+GameBattletab.newButton("no cooldown", "No cooldown on moves", function()
+    print('Hello!')
+end)
